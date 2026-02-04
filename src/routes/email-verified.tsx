b92f46c0ee40daf364484d467fc829bfcffdf837
Yaswanth1832K <yaswanthjallipalli@gmail.com>
2026-02-05 01:36:43 +0530

    Empty main branch

--- src/routes/email-verified.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/email-verified')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/email-verified"!</div>
-}

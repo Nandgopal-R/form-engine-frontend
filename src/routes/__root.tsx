ab126c006974a8fa7f923c8ea4bc946dc3d08ab5
adarsshkg-codebank <akgcodebank@gmail.com>
2026-02-06 07:23:39 +0530

    feat:Tanstack query setup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,19 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
 
-export const Route = createRootRoute({
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-left" />
-    </QueryClientProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   ),
 })
